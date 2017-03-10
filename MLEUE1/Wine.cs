69443c7393cfd45f404c40c040cbf27a2edfa377
viktoria paschinger <email>
2017-03-10 14:14:37 +0100

    added wine class

---
+++ MLEUE1/Wine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLEUE1
+{
+    class Wine
+    {
+        public double FixedAcidity { get; set; }
+        public double VolatileAcidity { get; set; }
+        public double CitricAcid { get; set; }
+        public double ResidualSugar { get; set; }
+        public double Chlorides { get; set; }
+        public double FreeSulfurDioxide { get; set; }
+        public double TotalSulfurDioxide { get; set; }
+        public double Density { get; set; }
+        public double PH { get; set; }
+        public double Sulphates { get; set; }
+        public double Alcohol { get; set; }
+        public int Quality { get; set; }
+    }
+}
